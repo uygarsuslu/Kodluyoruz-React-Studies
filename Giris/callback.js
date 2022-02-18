992a84968d92ddfbc295502bb1bd3178669c3867
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-18 23:01:04 +0300

    callback function and async await added

--- Giris/callback.js
+++ Giris/callback.js
@@ -18,8 +18,48 @@
 // sayHi( () => { console.log("Hello") } )
 // import { user, users } from "./my-module";
 
+
+// javascript'te herhangi bir veri kaynağına gidip veri çeken fonksiyon //
 import fetch from "node-fetch";
 
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then((data) => data.json())
-    .then((users) => console.log(users))
+// AŞAĞIDA ASYNC - AWAIT ILE DAHA DUZGUN KULLANIMI YER ALIYOR //
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((data) => data.json()) // api'da ne varsa onu getirir //
+//     .then((users) => {
+//         console.log("Users Yüklendi", users)
+
+//         fetch("https://jsonplaceholder.typicode.com/posts/1")
+//             .then((data) => data.json())
+//             .then((posts) => {
+//                 console.log("Post 1 Yüklendi", posts)
+
+//                 fetch("https://jsonplaceholder.typicode.com/posts/2")
+//                     .then((data) => data.json())
+//                     .then((data) => console.log("Post 2 Yüklendi", data))
+//             })
+//     })
+
+// ASYNC - AWAIT //
+// await yazmazsak o satırda beklemez ve bir sonraki satıra devam eder //
+// async function getData() {
+//     const users = await (await fetch("https://jsonplaceholder.typicode.com/users")).json()
+//     const post1 = await (await fetch("https://jsonplaceholder.typicode.com/posts/1")).json()
+//     const post2 = await (await fetch("https://jsonplaceholder.typicode.com/posts/2")).json()
+
+//     console.log("users", users)
+//     console.log("post1", post1)
+//     console.log("post2", post2)
+    
+// }
+// getData()
+
+// javascript'te anonim foksiyon oluşturma () () //
+( async() =>{
+    const users = await (await fetch("https://jsonplaceholder.typicode.com/users")).json()
+    const post1 = await (await fetch("https://jsonplaceholder.typicode.com/posts/1")).json()
+    const post2 = await (await fetch("https://jsonplaceholder.typicode.com/posts/2")).json()
+
+    console.log("users", users)
+    console.log("post1", post1)
+    console.log("post2", post2)
+}) () ;
