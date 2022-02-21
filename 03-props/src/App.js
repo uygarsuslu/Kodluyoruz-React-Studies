927381d219524d0a2d2c1ef1acb1fe2a4af13013
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-21 15:38:40 +0300

    dongulerde key propu added

--- 03-props/src/App.js
+++ 03-props/src/App.js
@@ -1,13 +1,63 @@
+// PROPS NEDİR? NASIL KULLANILIR? //
+
+// import './App.css';
+
+// import User from './components/User';
+
+// function App() {
+//   return (
+//     <>
+//       <User name="Mehmet" surname="Seven" isLoggedIn={true} age={29} />
+//     </>
+//   );
+// }
+
+// export default App;
+
+/* ************************************************************************************************************ */
+
+// DÖNGÜLERDE "KEY" PROP'U //
+
 import './App.css';
 
 import User from './components/User';
 
+const friends = [
+  {
+    id:1,
+    name: "Ahmet"
+  },
+  {
+    id:2,
+    name: "Mehmet"
+  },
+  {
+    id:3,
+    name: "Tayfun"
+  },
+  {
+    id:4,
+    name: "Gökhan"
+  },
+  {
+    id:5,
+    name: "Uygar"
+  },
+]
+
 function App() {
   return (
     <>
-      <User name="Mehmet" surname="Seven" isLoggedIn={true} age={29} />
+      <User
+        name="Mehmet"
+        surname="Seven"
+        isLoggedIn={true}
+        age={29}
+        friends={friends} />
     </>
   );
 }
 
 export default App;
+
+/* ************************************************************************************************************ */
--- 03-props/src/components/User.js
+++ 03-props/src/components/User.js
@@ -1,14 +1,47 @@
-function User({name, surname, isLoggedIn, age}) { // (props) //
+// PROPS NEDİR? NASIL KULLANILIR? //
+
+// function User({name, surname, isLoggedIn, age}) { // (props) //
+//     return (
+//         <h1>
+//             {
+//                 isLoggedIn ? `${name} ${surname} ${age}` : "Giris Yapmadınız"
+//                 // props.isLoggedIn ? `${props.name} ${props.surname}` : "Giris Yapmadınız"
+//             }
+//         </h1>
+//     )
+// }
+// export default User
+
+
+/* ************************************************************************************************************ */
+
+// DÖNGÜLERDE "KEY" PROP'U //
+
+function User({ name, surname, isLoggedIn, age, friends }) { // (props) //
     return (
-        <h1>
+        <>
+            <h1>
+                {
+                    isLoggedIn ? `${name} ${surname} ${age}` : "Giris Yapmadınız"
+                    // props.isLoggedIn ? `${props.name} ${props.surname}` : "Giris Yapmadınız"
+                }
+            </h1>
+
             {
-                isLoggedIn ? `${name} ${surname} (
-                    
-                )` : "Giris Yapmadınız"
-                // props.isLoggedIn ? `${props.name} ${props.surname}` : "Giris Yapmadınız"
+                friends.map((friend) => (
+                    <div key={friend.id}>{friend.name}</div>
+                ))
+
+                /*
+                friends.map((friend) => {
+                    const x = friend.id + 2
+                    return <div key={friend.id}>{friend.name}</div>
+                })
+                */
             }
-            
-        </h1>
+        </>
     )
 }
 export default User
+
+/* ************************************************************************************************************ */
