228a63e6c6c92a5fe701a670b35404ff41f97203
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-22 21:26:32 +0300

    component unmount added

---
+++ 05-lifecycle/src/components/Counter.js
@@ -0,0 +1,39 @@
+// CONPONENT UNMOUNT //
+
+// component'i unmount ettikten sonra o component'i tekrardan güncellemeye çalışabilecek ne varsa o işlemleri durdurur //
+
+import { useState, useEffect } from "react";
+
+function Counter() {
+
+    const [number, setNumber] = useState(0)
+
+    // if yapısının içinde çalışmaz - useEffect ve useState //
+    useEffect(() => {
+        console.log("Component mount edildi")
+
+        const interval = setInterval(() => {
+            // n parametresi number'ın o anda ifade etmiş olduğu değer //
+            setNumber((n) => n + 1);
+        }, 1000)
+
+        // componenet unmount edildiği anda setInterval'i durdurmamız gerekiyor //
+        // return () => console.log("Component unmount edildi");
+
+        // interval'i durdurma kısmı //
+        return () => clearInterval(interval)
+
+    }, []) // dependece array // // boş bırakılırsa = component mount edildiği anı yakala //
+
+    useEffect(() => {
+        console.log("number state guncellendi")
+    }, [number])
+
+    return (
+        <div>
+            <h1>{number} </h1>
+        </div >
+    )
+}
+export default Counter
+
