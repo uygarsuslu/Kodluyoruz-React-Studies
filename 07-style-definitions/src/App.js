2f83fea9b85cabb87b7405a40634f318e1540278
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-23 17:08:20 +0300

    style definitions added

---
+++ 07-style-definitions/src/App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+import A from "./components/A";
+import B from "./components/B";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+
+<A />
+<B />
+
+
+
+
+
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <div style={{ color: "red", backgroundColor: "white", paddingTop: 50 }} >Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, ex placeat rerum temporibus odio delectus veritatis cum alias libero accusantium velit iure enim laboriosam exercitationem illum quia sapiente voluptas ducimus.</div> */}
+
+        {/* cdnjs'den bootstrap kodu alıp public altındaki index.html dosyasına ekledikten sonra bunu yaptık */}
+        {/* <div class="alert alert-primary" role="alert">
+          A simple primary alert—check it out!
+        </div> */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
