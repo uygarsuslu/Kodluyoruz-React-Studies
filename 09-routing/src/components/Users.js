1e1b65d90f1e18a972b6d41bee48391d994c2f55
Uygar Süslü <uygar.suslu@santsg.com>
2022-02-25 17:31:24 +0300

    users-js added

---
+++ 09-routing/src/components/Users.js
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+function Users() {
+  const [loading, setLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      {loading && <div>Loading...</div>}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/user/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+
+      {/* <ul>
+        <li>
+          <Link to="/user/1">User 1</Link>
+        </li>
+        <li>
+          <Link to="/user/2">User 2</Link>
+        </li>
+        <li>
+          <Link to="/user/3">User 3</Link>
+        </li>
+      </ul> */}
+    </div>
+  );
+}
+
+export default Users;
