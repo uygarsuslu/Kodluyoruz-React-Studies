6c65496463cb43448329212667d1199f0135c0d1
Uygar Süslü <uygarsuslu1@gmail.com>
2022-02-28 22:17:22 +0300

    handle submit and handle change added

---
+++ 10-formik/src/components/HandleSubmit-HandleChange.js
@@ -0,0 +1,49 @@
+// ********** handleSubmit & handleChange ********** 
+
+// import işlemi //
+import { Formik } from 'formik';
+
+function Handle() {
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <Formik
+                initialValues={{
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                }}
+                onSubmit={async (values) => {
+                    alert(JSON.stringify(values, null, 2));
+                }}
+            >
+                {
+                    ({ handleSubmit, handleChange }) => (
+                        <form onSubmit={handleSubmit}>
+
+                            <label htmlFor="firstName">First Name</label>
+                            <input name="firstName" onChange={handleChange} />
+
+                            <br />
+
+                            <label htmlFor="lastName">Last Name</label>
+                            <input name="lastName" onChange={handleChange} />
+
+                            <br />
+
+                            <label htmlFor="email">Email</label>
+                            <input name="email" type="email" onChange={handleChange} />
+
+                            <br />
+
+                            <button type="submit">Submit</button>
+
+                        </form>
+                    )
+                }
+            </Formik>
+        </div>
+    );
+}
+
+export default Handle;
