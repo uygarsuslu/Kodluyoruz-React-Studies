c316aac6905d54a6d4c5190f497fe8565c601580
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-02 20:09:54 +0300

    context provider - theme switcher added

---
+++ 12-context-api/src/components/Header.js
@@ -0,0 +1,39 @@
+// ********** CONTEXT PROVİDER ********** //
+
+// import { useContext } from 'react'
+
+// import ThemeContext from "../context/ThemeContext";
+
+// function Header() {
+
+//     const { theme, setTheme } = useContext(ThemeContext)
+
+//     return (
+//         <div>Active Theme: {theme}
+//             <br />
+//             <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>Change Theme</button>
+//         </div>
+//     )
+// }
+// export default Header
+
+/* ******************************************************************************************************* */
+
+// ********** THEME SWİTCHER ********** //
+
+import { useContext } from 'react'
+
+import ThemeContext from "../context/ThemeContext";
+
+function Header() {
+
+    const { theme, setTheme } = useContext(ThemeContext)
+
+    return (
+        <div>Active Theme: {theme}
+            <br />
+            <button onClick={() => setTheme(theme === "light" ? "dark" : "light")}>Change Theme</button>
+        </div>
+    )
+}
+export default Header
