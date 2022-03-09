5b52c55c47fbc72fc21c08ad40310e1e41bc0b75
Uygar Süslü <uygarsuslu1@gmail.com>
2022-03-09 20:25:26 +0300

    testing added

---
+++ 15-testing/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+// import Counter from "./components/Counter";
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Counter /> */}
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
+
+// her bir birimin kendi içinde doğru çalışıp çalışmadığını otomatize ettirecek kodu yazdırabiliriz //
+// her component'in kendi içinde doğru çalıştığına emin olmamız gerek //
