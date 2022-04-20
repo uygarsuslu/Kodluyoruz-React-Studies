93c040b212fed0ff2d400b37517babec4901db8b
Uygar Süslü <uygarsuslu1@gmail.com>
2022-04-20 19:39:28 +0300

    props added

--- 03-props/src/App.js
+++ 03-props/src/App.js
@@ -60,58 +60,12 @@
 
 /* ************************************************************************************************************ */
 
-// PROP TYPES - PROP TYPES: isRequired //
+// PROP TYPES - PROP TYPES: isRequired - PROP TYPES: onOfType - PROP TYPES: shape //
 
 // prop types = componentlere gönderdiğimiz propertylerin tiplerini belirleyebileceğimiz araç //  
 // isRequired = prop tiplerini yazarken zorunlu olanlarıda belirtebiliyoruz //
-
-// import './App.css';
-
-// import User from './components/User';
-
-// const friends = [
-//   {
-//     id: 1,
-//     name: "Ahmet"
-//   },
-//   {
-//     id: 2,
-//     name: "Mehmet"
-//   },
-//   {
-//     id: 3,
-//     name: "Tayfun"
-//   },
-//   {
-//     id: 4,
-//     name: "Gökhan"
-//   },
-//   {
-//     id: 5,
-//     name: "Uygar"
-//   },
-// ]
-
-// function App() {
-//   return (
-//     <>
-//       <User
-//         name="Mehmet"
-//         surname="Seven"
-//         isLoggedIn={true}
-//         age={29}
-//         //  friends={friends} // map'in başına "friends &&" koyarak hata almamayı sağladık //
-//       />
-//     </>
-//   );
-// }
-// export default App;
-
-/* ************************************************************************************************************ */
-
-// PROP TYPES: onOfType //
-
 // oneOfType = bir property'de birden fazla veri tipinin gönderilmesine imkan tanır //
+// shape = obje olarak gönderdiğimiz propertylerde kullanırız // 
 
 // import './App.css';
 
@@ -148,55 +102,7 @@
 //         surname="Seven"
 //         isLoggedIn={true}
 //         age={"29"}
-//         friends={friends} 
-//       />
-//     </>
-//   );
-// }
-// export default App;
-
-/* ************************************************************************************************************ */
-
-// PROP TYPES: shape //
-
-// obje olarak gönderdiğimiz propertylerde kullanırız // 
-
-// import './App.css';
-
-// import User from './components/User';
-
-// const friends = [
-//   {
-//     id: 1,
-//     name: "Ahmet"
-//   },
-//   {
-//     id: 2,
-//     name: "Mehmet"
-//   },
-//   {
-//     id: 3,
-//     name: "Tayfun"
-//   },
-//   {
-//     id: 4,
-//     name: "Gökhan"
-//   },
-//   {
-//     id: 5,
-//     name: "Uygar"
-//   },
-// ]
-
-// function App() {
-//   return (
-//     <>
-//       <User
-//         name="Mehmet"
-//         surname="Seven"
-//         isLoggedIn={true}
-//         age={"29"}
-//         friends={friends} 
+//         //  friends={friends} // map'in başına "friends &&" koyarak hata almamayı sağladık //
 //         address={{
 //           title:"Kartal/Istanbul",
 //           zip: 34755
@@ -216,42 +122,14 @@ import './App.css';
 
 import User from './components/User';
 
-const friends = [
-  {
-    id: 1,
-    name: "Ahmet"
-  },
-  {
-    id: 2,
-    name: "Mehmet"
-  },
-  {
-    id: 3,
-    name: "Tayfun"
-  },
-  {
-    id: 4,
-    name: "Gökhan"
-  },
-  {
-    id: 5,
-    name: "Uygar"
-  },
-]
-
 function App() {
   return (
     <>
       <User
         // name="Mehmet"
         surname="Seven"
-        isLoggedIn={true}
+        //isLoggedIn={true}
         age={"29"}
-        friends={friends} 
-        address={{
-          title:"Kartal/Istanbul",
-          zip: 34755
-        }}
       />
     </>
   );
--- 03-props/src/components/User.js
+++ 03-props/src/components/User.js
@@ -12,9 +12,7 @@
 // }
 // export default User
 
-
 /* ************************************************************************************************************ */
-
 
 // DÖNGÜLERDE "KEY" PROP'U //
 // div içinde key verdik. bunu çalıştırdığımız her döngü listelemesinde mutlaka en dıştaki elemana vermemiz gerekecek. //
@@ -46,88 +44,15 @@
 // }
 // export default User
 
-
 /* ************************************************************************************************************ */
 
 // PROP TYPES */* PROP TYPES: isRequired */* PROP TYPES: oneOfType //
 
 // prop types = componentlere gönderdiğimiz propertylerin tiplerini belirleyebileceğimiz araç //  
-// isRequired = prop tiplerini yazarken zorunlu olanlarıda belirtebiliyoruz //
-
-// import PropTypes from "prop-types"
-
-// function User({ name, surname, isLoggedIn, age, friends }) { // (props) //
-//     return (
-//         <>
-//             <h1>
-//                 {
-//                     isLoggedIn ? `${name} ${surname} ${age}` : "Giris Yapmadınız"
-//                     // props.isLoggedIn ? `${props.name} ${props.surname}` : "Giris Yapmadınız"
-//                 }
-//             </h1>
-
-//             {
-//                 friends && // varsa göster demek //
-//                 friends.map((friend) => (
-//                     <div key={friend.id}>{friend.name}</div>
-//                 ))
-//             }
-//         </>
-//     )
-// }
-
-// // BU KISMI DOĞRU YAPMAZSAK CONSOLE'DA HATA VERİR //
-// User.propTypes = {
-//     name: PropTypes.string.isRequired, // zorunlu //
-//     surname: PropTypes.string.isRequired,
-//     isLoggedIn: PropTypes.bool.isRequired,
-//     age: PropTypes.number.isRequired,
-//     friends: PropTypes.array
-// }
-// export default User
-
-/* ************************************************************************************************************ */
-
-// PROP TYPES: oneOfType //
+// isRequired = prop tiplerini yazarken zorunlu olanları belirtebiliyoruz //
 // oneOfType = bir property'de birden fazla veri tipinin gönderilmesine imkan tanır //
 // yani biz oneOfType'ı kullanınca hem "string" olarak hemde "number" olarak tanımladığım için 2 türlü de yazsam app.jsdeki ilgili yere hata almıyorum. Zorunlu kılmak için ise parantezlerin sonuna isRequired tanımını girmek yeterli olacaktır //
-
-// import PropTypes from "prop-types"
-
-// function User({ name, surname, isLoggedIn, age, friends }) { // (props) //
-//     return (
-//         <>
-//             <h1>
-//                 {
-//                     isLoggedIn ? `${name} ${surname} ${age}` : "Giris Yapmadınız"
-//                     // props.isLoggedIn ? `${props.name} ${props.surname}` : "Giris Yapmadınız"
-//                 }
-//             </h1>
-
-//             {
-//                 friends && // varsa göster demek //
-//                 friends.map((friend) => (
-//                     <div key={friend.id}>{friend.name}</div>
-//                 ))
-//             }
-//         </>
-//     )
-// }
-
-// // BU KISMI DOĞRU YAPMAZSAK CONSOLE'DA HATA VERİR //
-// User.propTypes = {
-//     name: PropTypes.string.isRequired, // zorunlu //
-//     surname: PropTypes.string.isRequired,
-//     isLoggedIn: PropTypes.bool.isRequired,
-//     age: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-//     friends: PropTypes.array
-// }
-// export default User
-
-/* ************************************************************************************************************ */
-
-// PROP TYPES: shape //
-// obje olarak gönderdiğimiz propertylerde kullanırız // 
+// shape = obje olarak gönderdiğimiz propertylerde kullanırız //   
 
 // import PropTypes from "prop-types"
 
@@ -174,7 +99,7 @@
 
 import PropTypes from "prop-types"
 
-function User({ name, surname, isLoggedIn, age, friends, address }) { // (props) //
+function User({ name, surname, isLoggedIn, age }) { // (props) //
 
     // bunu dışarda tanımlamamızın sebebi eğer bilgi yoksa ad soyad bilgilerini ekrana getirmesin //
     if (!isLoggedIn) {
@@ -186,30 +111,14 @@ function User({ name, surname, isLoggedIn, age, friends, address }) { // (props)
             <h1>
                 {`${name} ${surname} (${age})`}
             </h1>
-
-            <h4>{address.title} {address.zip}</h4>
-
-            {
-                friends && // varsa göster demek //
-                friends.map((friend) => (
-                    <div key={friend.id}>{friend.name}</div>
-                ))
-            }
         </>
     )
 }
 
 // BU KISMI DOĞRU YAPMAZSAK CONSOLE'DA HATA VERİR //
 User.propTypes = {
-    name: PropTypes.string.isRequired, // zorunlu //
-    surname: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
     isLoggedIn: PropTypes.bool.isRequired,
-    age: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
-    friends: PropTypes.array,
-    address: PropTypes.shape({
-        title: PropTypes.string,
-        zip: PropTypes.number
-    })
 }
 
 User.defaultProps = {
