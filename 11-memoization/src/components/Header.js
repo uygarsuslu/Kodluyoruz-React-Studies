7bd51e4b4d4eacc261906451e6040a26e7a2ae9d
Uygar Süslü <uygarsuslu1@gmail.com>
2022-04-30 20:26:33 +0300

    react.memo - usememo - usecallback added

--- 11-memoization/src/components/Header.js
+++ 11-memoization/src/components/Header.js
@@ -14,17 +14,20 @@
 
 // ********** USEMEMO - FONKSİYONLU ÖRNEK ********** //
 
+// boş [] ve {} ≠ boş [] ve {}
+
 // import React from 'react'
 
 // function Header({ number, data }) {
 //     console.log("Header Component Re-rendered");
 //     return (
-//         <div>Header - {number}
-//         <br /><br />
-//         <code>{JSON.stringify(data)}</code>
+//         <div>
+//             Header
+//             <br /><br />
+//             <code>{JSON.stringify(data)}</code>
 //         </div>
-//         )
-//     }
+//     )
+// }
 // export default React.memo(Header)
 
 /* ************************************************************************************************* */
@@ -36,10 +39,11 @@ import React from 'react'
 function Header({ number, increment }) {
     console.log("Header Component Re-rendered");
     return (
-        <div>Header - {number}
-        <br /><br />
-        <button onClick={increment}>Click</button>
+        <div>
+            Header
+            <br /><br />
+            <button onClick={increment}>Click</button>
         </div>
-        )
+    )
 }
 export default React.memo(Header)
