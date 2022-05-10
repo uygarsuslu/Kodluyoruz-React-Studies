3eee5f8614b997dce9c4770a4e42971d5cb5c364
Uygar Süslü <uygarsuslu1@gmail.com>
2022-05-10 15:31:35 +0300

    palette component added

---
+++ 13-realtime-apps/client/src/components/Palette.js
@@ -0,0 +1,22 @@
+// butona basıldığında renk gönderilmeli ama önce rengin ne olduğunu öğrenmek için kullandık
+import { useState } from 'react'
+
+import { send } from "../socketApi";
+
+function Palette({ activeColor }) {
+
+    const [color, setColor] = useState("#000")
+
+    return (
+        <div className='palette'>
+            <input
+                type="color"
+                value={activeColor}
+                onChange={(e) => setColor(e.target.value)}
+            />
+            <button onClick={() => send(color)}>Click</button>
+        </div>
+    )
+}
+
+export default Palette
